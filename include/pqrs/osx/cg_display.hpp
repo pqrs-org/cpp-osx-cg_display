1a7c5ec0c861ef14cda9e5671ecca6537349e0f5
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-21 20:33:27 +0900

    v1.3.0

--- include/pqrs/osx/cg_display.hpp
+++ include/pqrs/osx/cg_display.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::cg_display v1.2
+// pqrs::osx::cg_display v1.3.0
 
 // (C) Copyright Takayama Fumihiko 2021.
 // Distributed under the Boost Software License, Version 1.0.
