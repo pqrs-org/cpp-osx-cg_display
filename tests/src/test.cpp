4c2a2ec3d2eb0bd4182877c17f2a4decb633f7b7
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-21 20:32:55 +0900

    Add [[nodiscard]]

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,3 +1,15 @@
-int main(void) {
+#include <concepts>
+#include <cstdint>
+#include <optional>
+#include <pqrs/osx/cg_display.hpp>
+#include <vector>
+
+static_assert(std::same_as<decltype(pqrs::osx::cg_display::online_display_count()), uint32_t>);
+static_assert(std::same_as<decltype(pqrs::osx::cg_display::online_displays()), std::vector<CGDirectDisplayID>>);
+static_assert(std::same_as<decltype(pqrs::osx::cg_display::active_display_count()), uint32_t>);
+static_assert(std::same_as<decltype(pqrs::osx::cg_display::active_displays()), std::vector<CGDirectDisplayID>>);
+static_assert(std::same_as<decltype(pqrs::osx::cg_display::get_online_display_id_by_mouse_cursor()), std::optional<CGDirectDisplayID>>);
+
+int main() {
   return 0;
 }
